cf4f43e240e19124d088094880df6d6abe8f4662
Junsung <031wnstjd@naver.com>
2023-10-06 22:11:49 +0900

    Response 데이터 제어를 위한 Filtering

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -9,14 +10,13 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@JsonIgnoreProperties(value={"password", "ssn"})
 public class User {
-
     private Integer id;
-
     @Size(min = 2, message = "Name은 2글자 이상 입력해주세요.")
     private String name;
-
     @Past
     private Date joinDate;
-
+    private String password;
+    private String ssn;
 }
